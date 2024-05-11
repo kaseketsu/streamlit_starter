13f7fbdcffb66341e6a208adcd4c040e4e523cb5
kaseketsu <168079236+kaseketsu@users.noreply.github.com>
2024-05-11 11:24:38 +0800

    a simple demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,15 +8,16 @@ if 'messages' not in st.session_state:
     st.session_state['messages'] = [{'role':'assistant','content':'How are you today'}]
 for msg in st.session_state['messages']:
     st.chat_message(msg['role']).write(msg['content'])
-if prompt := st.chat_input():
+prompt = st.chat_input()
+if prompt is not None:
     if not openai_api_key:
         st.info("error:请输入open_ai_api_key")
         st.stop()
-openai.api_key = openai_api_key
-st.session_state['messages'].append({'role':'user','content':prompt})
-st.chat_message('user').write(prompt)
-response = openai.ChatCompletion.create(model = 'gpt-3.5-turbo',messages = st.session_state['messages'],
+    openai.api_key = openai_api_key
+    st.session_state['messages'].append({'role':'user','content':prompt})
+    st.chat_message('user').write(prompt)
+    response = openai.ChatCompletion.create(model = 'gpt-3.5-turbo',messages = st.session_state['messages'],
                                         tmperature = 0.7,max_tokens = 1000)
-answer = response['choices'][0].message.content
-st.session_state['messages'].append({'role':'assistant','content':answer})
-st.chat_message('system').write(answer)
+    answer = response['choices'][0].message.content
+    st.session_state['messages'].append({'role':'assistant','content':answer})
+    st.chat_message('system').write(answer)
